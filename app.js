1ae33bca9f5e9c23173cae3215fec0d0350eda83
Ivan Perez <Ivan@Mac-mini-de-Ivan.local>
2015-03-10 09:12:37 -0600

    fixes

--- app.js
+++ app.js
@@ -63,9 +63,10 @@ app.use(function(err, req, res, next) {
     });
 });
 
-app.listen(process.env.PORT || 3000,function(e){
-    if(e) throw e;
-    console.log('Express server listening on',process.env.PORT);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
 });
+
 
 //module.exports = app;
